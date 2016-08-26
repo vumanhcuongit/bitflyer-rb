c8d1caebe7ee595919f7f63bf3e2bde351d88a69
Cuong Vu <cuongvu0103@gmail.com>
2016-08-26 16:50:03 +0700

    Change object name

--- lib/bitflyer/executions.rb
+++ lib/bitflyer/executions.rb
@@ -2,8 +2,8 @@ module Bitflyer
   module Executions
     def self.all(options = {})
       result = Bitflyer::Net.get("/v1/executions", options)
-      JSON.parse(result).map do |trade_attr|
-        Bitflyer::Execution.new(trade_attr)
+      JSON.parse(result).map do |exectuion|
+        Bitflyer::Execution.new(exectuion)
       end
     end
   end
