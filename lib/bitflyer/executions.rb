f2f03eaf5be27dad91d39925d6425f6dfc47a598
Cuong Vu <cuongvu0103@gmail.com>
2016-08-26 16:50:34 +0700

    Correct typo mistake

--- lib/bitflyer/executions.rb
+++ lib/bitflyer/executions.rb
@@ -2,8 +2,8 @@ module Bitflyer
   module Executions
     def self.all(options = {})
       result = Bitflyer::Net.get("/v1/executions", options)
-      JSON.parse(result).map do |exectuion|
-        Bitflyer::Execution.new(exectuion)
+      JSON.parse(result).map do |execution|
+        Bitflyer::Execution.new(execution)
       end
     end
   end
