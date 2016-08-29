d3404aec4de2a3e62a66f75c4a347993f52e42c3
Cuong Vu <cuongvu0103@gmail.com>
2016-08-29 19:30:22 +0700

    Get my executions

--- lib/bitflyer/executions.rb
+++ lib/bitflyer/executions.rb
@@ -6,5 +6,12 @@ module Bitflyer
         Bitflyer::Execution.new(execution)
       end
     end
+
+    def self.me(options = {})
+      executions = Bitflyer::Net.get("/v1/me/getexecutions", options, true)
+      JSON.parse(executions).map do |execution|
+        Bitflyer::Execution.new(execution)
+      end
+    end
   end
 end
